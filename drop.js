73bb70130d2452bd41dc41d7ecad4f0ec6afca56
Max Lefebvre <m4xle5@gmail.com>
2017-04-12 21:16:06 -0300

    changed to jquery extension - also tested

--- drop.js
+++ drop.js
@@ -1,64 +1,52 @@
 (function (window) {
-    // define library methods and properties
-    function definition() {
-        var lib = {};
-        
-        // initialize drop.js
-        lib.init = function (map, itemsSelector) {
-            lib.map = map;
-            $(itemsSelector).draggable({
-                helper: clone,
+    jQuery.fn.extend({
+        drop: function (map, callback) {
+            // make element draggable
+            this.draggable({
+                helper: 'clone',
+                start: function (e, ui) {
+                    debugger;
+                    var original = $(ui.helper);
+                    var edited = $(ui.helper);
+                    edited.css({ 
+                        "width": "50px", 
+                        "height": "50px",
+                        "margin-left": e.clientX - $(e.target).offset().left - 25,
+                        "margin-top": e.clientY - $(e.target).offset().top - 25 
+                    });
+                },
                 stop: function (e, ui) {
                     var marker = DropMarker(e, ui);
+                    if (callback) {
+                        callback(marker);
+                    }
                 }
             })
-            console.log('successfully initialized pindrop.js');
-        };
 
-        // handle dropped marker
-        function DropMarker(e, ui) {
-            var wrapper = lib.map.getDiv().offset();
-            var x = ui.helper.offset().left - wrapper.left + parseInt("20px", 10);
-            var y = ui.helper.offset().top - wrapper.top + parseInt("40px", 10);
-            var url = $(ui.helper).find('img')[0].src;
-            var icon = new window.google.maps.MarkerImage(url, null, null, null, new window.google.maps.Size(40, 40));
-            return new window.google.maps.Marker({
-                map: lib.map,
-                position: screenPointToLatLng({ x: x, y: y }, lib.map),
-                draggable: true
-            });
+            // handle dropped marker
+            function DropMarker(e, ui) {
+                var wrapper = $(map.getDiv()).offset();
+                var x = ui.helper.offset().left - wrapper.left + $(ui.helper).width() / 2;
+                var y = ui.helper.offset().top - wrapper.top + $(ui.helper).height();
+                var url = $(ui.helper)[0].src;
+                var icon = new window.google.maps.MarkerImage(url, null, null, null, new window.google.maps.Size(40, 40));
+                return new window.google.maps.Marker({
+                    map: map,
+                    position: screenPointToLatLng({ x: x, y: y }),
+                    draggable: true,
+                    icon: icon
+                });
+            }
+
+            // convert x,y screen position to lat,lng
+            function screenPointToLatLng(point) {
+                var topRight = map.getProjection().fromLatLngToPoint(map.getBounds().getNorthEast());
+                var bottomLeft = map.getProjection().fromLatLngToPoint(map.getBounds().getSouthWest());
+                var scale = Math.pow(2, map.getZoom());
+                var worldPoint = new window.google.maps.Point(point.x / scale + bottomLeft.x, point.y / scale + topRight.y);
+                return map.getProjection().fromPointToLatLng(worldPoint);
+            }
+
         }
-
-        // convert x,y screen position to lat,lng
-        function screenPointToLatLng(point, map) {
-            var topRight = map.getProjection().fromLatLngToPoint(map.getBounds().getNorthEast());
-            var bottomLeft = map.getProjection().fromLatLngToPoint(map.getBounds().getSouthWest());
-            var scale = Math.pow(2, map.getZoom());
-            var worldPoint = new window.google.maps.Point(point.x / scale + bottomLeft.x, point.y / scale + topRight.y);
-            return map.getProjection().fromPointToLatLng(worldPoint);
-        }
-        return lib;
-    }
-
-    // check if jQuery is defined
-    if (typeof (jQuery) === 'undefined') {
-        console.error('drop.js requires jQuery. https://jquery.com/')
-        return;
-    }
-
-    // check if jQuery.ui is defined
-    if(typeof(jQuery.ui) === 'undefined'){
-        console.error('drop.js requires jQuery UI. https://jqueryui.com')
-    }
-
-    // check if google.maps is defined
-    if (typeof (google.maps) === 'undefined') {
-        console.error('drop.js requires the google maps API. https://developers.google.com/maps/')
-        return;
-    }
-
-    // define library globally
-    if (typeof (google.drop) === 'undefined') {
-        window.google.drop = definition();
-    }
+    });
 })(window);
